5dbd72048e9a332854eb6828b4e3269b148bb479
Petar Dimitrov <petar.dimitrov@bertelsmann.de>
2023-03-01 11:33:49 +0100

    Second Commit

---
+++ src/main/java/org/evonik/Main.java
@@ -0,0 +1,24 @@
+package org.evonik;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+/*
+.gradle
+/build/
+
+# Ignore Gradle GUI config
+gradle-app.setting
+
+# Avoid ignoring Gradle wrapper jar file (.jar files are usually ignored)
+!gradle-wrapper.jar
+
+# Cache of project
+.gradletasknamecache
+
+# # Work around https://youtrack.jetbrains.com/issue/IDEA-116898
+# gradle/wrapper/gradle-wrapper.properties
+ */
